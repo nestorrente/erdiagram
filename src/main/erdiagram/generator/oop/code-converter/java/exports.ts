0b32de3a1c585fa753505b82fce3642d262ecf92
Néstor Pérez González <nperez@continuumsecurity.net>
2021-02-05 22:02:46 +0100

    Ensure some functions are exported.

--- src/main/erdiagram/generator/oop/code-converter/java/exports.ts
+++ src/main/erdiagram/generator/oop/code-converter/java/exports.ts
@@ -1,5 +1,8 @@
-import JavaType from '@/erdiagram/generator/oop/code-converter/java/type/JavaType';
-import JavaParameterizedType from '@/erdiagram/generator/oop/code-converter/java/type/JavaParameterizedType';
+import JavaType, {createJavaType} from '@/erdiagram/generator/oop/code-converter/java/type/JavaType';
+import JavaParameterizedType, {
+	createJavaParameterizedType,
+	isParameterizedType
+} from '@/erdiagram/generator/oop/code-converter/java/type/JavaParameterizedType';
 import JavaClassModelToCodeConverter from '@/erdiagram/generator/oop/code-converter/java/JavaClassModelToCodeConverter';
 import JavaClassModelToCodeConverterConfig, {
 	defaultJavaClassModelToCodeConverterConfig,
@@ -9,7 +12,10 @@ import JavaClassModelToCodeConverterConfig, {
 
 export {
 	JavaType,
+	createJavaType,
 	JavaParameterizedType,
+	createJavaParameterizedType,
+	isParameterizedType,
 	JavaClassModelToCodeConverter,
 	JavaClassModelToCodeConverterConfig,
 	defaultJavaClassModelToCodeConverterConfig,
